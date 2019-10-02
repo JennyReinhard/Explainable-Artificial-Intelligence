d4d21de80a8f3eeea83f5826185d07c6642b43f1
Luca Liehner <luca.liehner@rwth-aachen.de>
2019-10-02 17:41:51 +0200

    Finishing up UIkit integration

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=70)
+    content = models.TextField()
+    purpose = models.CharField(max_length=70)
+    created = models.DateTimeField(auto_now=True)
+    
