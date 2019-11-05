9a4055951ab899f08787fc504582e4b4fabafc41
Luca Liehner <luca.liehner@rwth-aachen.de>
2019-11-05 11:44:59 +0100

    Added post

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Post
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(ImportExportModelAdmin):
+    pass
