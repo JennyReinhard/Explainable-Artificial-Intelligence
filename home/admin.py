a0772185a140dbb526b9703e576bc60443168ef2
Luca Liehner <luca.liehner@rwth-aachen.de>
2019-11-05 12:04:05 +0100

    removed post

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Post
-from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-@admin.register(Post)
+admin.site.register(Post)
