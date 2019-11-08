646806acecaa626b725c3d6795670c9237f85c27
Luca Liehner <luca.liehner@rwth-aachen.de>
2019-11-08 09:30:06 +0100

    Removed registration possibility

--- riskywally/urls.py
+++ riskywally/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls, name='admin'),
-    path('register/', account_views.register, name='register'),
+    # path('register/', account_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 
