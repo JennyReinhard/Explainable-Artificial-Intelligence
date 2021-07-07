27ec6a27d2e1f4d51472f8e5eadc12d2c948ff7b
JennyReinhard <jenny.reinhard@rwth-aachen.de>
2021-07-07 17:49:23 +0200

    updated urls

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.home, name='index'),
-    path('post/new/', login_required(views.PostCreateView.as_view()), name='new-post'),
-    path('post/<int:pk>/update/', login_required(views.PostUpdateView.as_view()), name="update-post")
+    #path('', views.home, name='index'),
+    #path('post/new/', login_required(views.PostCreateView.as_view()), name='new-post'),
+    #path('post/<int:pk>/update/', login_required(views.PostUpdateView.as_view()), name="update-post")
 ]
